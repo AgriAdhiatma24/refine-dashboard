ec0630e35838b1678671e5eebe44c070bec4de5b
unknown <agriadhiatma@gmail.com>
2024-11-08 12:43:51 +0700

    feat: custom data provider

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,7 @@ import {
   useNotificationProvider,
 } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
-
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from "@refinedev/nestjs-query";
+import { dataProvider, liveProvider } from "./providers/data";
 import routerBindings, {
   CatchAllNavigate,
   DocumentTitleHandler,
@@ -47,8 +43,8 @@ function App() {
         <AntdApp>
           <DevtoolsProvider>
             <Refine
-              // dataProvider={dataProvider(gqlClient)}
-              // liveProvider={liveProvider(wsClient)}
+              dataProvider={dataProvider}
+              liveProvider={liveProvider}
               notificationProvider={useNotificationProvider}
               routerProvider={routerBindings}
               // authProvider={}
